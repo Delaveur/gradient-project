0bd682fb5d7f1fc6c3fe9e4aac102cdb1ef7aaf4
Delaveur <delaveur@yandex.ru>
2025-04-25 17:10:52 +0300

    feat: add contact page

---
+++ contacts/index.php
@@ -0,0 +1,12 @@
+<?php
+$page_title = "Контакты";
+include($_SERVER["DOCUMENT_ROOT"] . "/inc/header.php"); ?>
+
+<div class="relative w-full h-full">
+  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A5c6aa6628aec462e4c8151d918b9e7f0a08887904fceedee924ecc3ad5214325&amp;source=constructor" width="100%" frameborder="0" class="w-full h-full min-h-[500px] rounded-2xl shadow-lg border-0" allowfullscreen="true" style="position:relative;"></iframe>
+  <div class="absolute bottom-12 left-2 bg-white bg-opacity-90 rounded-xl p-4 shadow-lg">
+    <p class="text-lg mb-1"><strong>Телефон: </strong>+7(926)112-31-58</p>
+    <p class="text-lg"><strong>Адрес: </strong>124678, Москва, ул. Лужнецкая набережная, д. 2/4, стр. 4</p>
+  </div>
+</div>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/inc/footer.php");  ?>
